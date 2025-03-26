96dd1982ddfd100b96f675a023276b55fe5cf4d9
jrodriguez-1 <jrod1527@gmail.com>
2025-03-26 15:28:31 -0500

    clean up main class and Docker file changes

--- src/main/java/com/example/ReceiptProcessor/ReceiptProcessorApplication.java
+++ src/main/java/com/example/ReceiptProcessor/ReceiptProcessorApplication.java
@@ -14,17 +14,10 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 import java.util.Map;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.ReceiptProcessor", "com.example.ReceiptProcessor.controller"})
 public class ReceiptProcessorApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(ReceiptProcessorApplication.class);
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(ReceiptProcessorApplication.class, args);
-
-		// Log all registered request mappings
-		RequestMappingHandlerMapping requestMappingHandlerMapping = context.getBean(RequestMappingHandlerMapping.class);
-		Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping.getHandlerMethods();
-		map.forEach((key, value) -> log.info("{} => {}", key, value));
+		SpringApplication.run(ReceiptProcessorApplication.class, args);
 	}
 }
